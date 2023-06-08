487c9516585af9b1647b403ceb064dfdaeaa4d51
NguyenLongBien <nguyenlongbien25501git@gmail.com>
2023-06-08 23:25:05 +0700

    DATNK15

---
+++ BACKEND/Extensions/Service/Error/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using BACKEND.Interface.IService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace BACKEND.Extensions.Service.Error
+{
+    public static class ExceptionMiddleware
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILoggerService logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Something went wrong: {contextFeature.Error}");
+
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+
+                            Message = "LỖI BÊN TRONG SERVER . HÃY KIỂM TRA LẠI !"
+
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ BACKEND/Interface/IData/IPZEM017/IPZEM017_Repository.cs
@@ -0,0 +1,25 @@
+﻿using BACKEND.Entities.DTO.DataDto.PZEM017;
+using BACKEND.Entities.Model.Data_Mo.PZEM017;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace BACKEND.Interface.IData.IPZEM017
+{
+    public interface IPZEM017_Repository
+    { //GET
+        Task<IEnumerable<PZEM017_Model>> GetAllPzem017Async(PZEM017_DataShapping_Dto repuestShapping, PZEM017_FillterTime_Dto fillter);
+
+        //GET ID
+        Task<PZEM017_Model> GetIdPzem017Async(Guid id); // Lấy  dữ liệu theo id 
+
+        //POST
+        Task CreatePzem017Async(PZEM017_Model data);
+
+        //PUT
+        Task UpdatePzem017Async(PZEM017_Model data);
+
+        //DELETE
+        Task DeletePzem017Async(Guid id);
+    }
+}
